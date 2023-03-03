18d8fd65c5440db396efd3851088df5989a7fb62
zhangtao <zhangtao@sensetime.com>
2023-03-03 18:48:23 +0800

    improve status btn

--- src/utils/updateStatusBarItem.ts
+++ src/utils/updateStatusBarItem.ts
@@ -7,7 +7,9 @@ export async function updateStatusBarItem(
     extraText: string
 ): Promise<void> {
     let text = "$(lightbulb-autofix)"
-    let tooltip = "Auto";
+    let tip = new vscode.MarkdownString;
+    tip.isTrusted = true;
+    let mode = "[Auto](command:sensecode.inlineSuggest.toggle)";
     statusBarItem.show();
     if (statusbartimer) {
         clearTimeout(statusbartimer);
@@ -16,9 +18,23 @@ export async function updateStatusBarItem(
     let autoComplete = configuration.get("CompletionAutomatically", true);
     if (!autoComplete) {
         text = "$(lightbulb)"
-        tooltip = 'Manual';
+        mode = '[Alt+/](command:sensecode.inlineSuggest.toggle)';
     }
-    statusBarItem.tooltip = "SenseCode: " + tooltip;
+
+    let engine = "SenseCore";
+    let url = configuration.get("EngineAPI", "");
+    if (url.includes("code.sensecore")) {
+        engine = "SenseCode";
+    } else if (url.includes("tianqi")) {
+        engine = "TianQi";
+    }
+
+    tip.appendMarkdown(`**SenseCode**\n\n`);
+    tip.appendMarkdown(`***\n\n`);
+    tip.appendMarkdown(`Engine: ${engine}\n\n`);
+    tip.appendMarkdown(`Trigger: ${mode}\n\n`);
+
+    statusBarItem.tooltip = tip;
     statusBarItem.text = text;
     if (extraText) {
         statusBarItem.text = `${text} ${extraText}`;
