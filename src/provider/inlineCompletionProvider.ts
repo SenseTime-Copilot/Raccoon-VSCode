0f03f44b31526c540bfc832b09bc4177653a02b1
zhangtao <zhangtao@sensetime.com>
2023-08-01 16:51:23 +0800

    fix tip text

--- src/provider/inlineCompletionProvider.ts
+++ src/provider/inlineCompletionProvider.ts
@@ -40,7 +40,7 @@ export function inlineCompletionProvider(
           statusBarItem,
           {
             text: "$(workspace-untrusted)",
-            tooltip: vscode.l10n.t("User cancelled")
+            tooltip: vscode.l10n.t("Unauthorized")
           }
         );
         return;
