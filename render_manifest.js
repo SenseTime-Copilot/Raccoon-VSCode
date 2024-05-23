a9ee9518155d5a87bae6069b0aebb7bdc08ea53b
zhangtao <zhangtao@sensetime.com>
2024-05-23 10:25:28 +0800

    Add value file CI config

--- render_manifest.js
+++ render_manifest.js
@@ -3,13 +3,16 @@ const fs = require('node:fs');
 
 let packageId = args.packageId || "raccoon";
 let packageName = args.packageName || "Raccoon";
+let packageValueFile = args.packageValueFile || undefined;
 let packageType = args.packageType || "Standard";
+let webBaseUrl = args.webBaseUrl || "https://raccoon.sensetime.com";
 let apiBaseUrl = args.apiBaseUrl || "https://raccoon-api.sensetime.com/api/plugin";
 
 console.log("=============== Rendering Settings ===============");
 console.log(` Package ID   : ${packageId}`);
 console.log(` Package Name : ${packageName}`);
 console.log(` Package Type : ${packageType}`);
+console.log(` Web Base URL : ${webBaseUrl}`);
 console.log(` API Base URL : ${apiBaseUrl}`);
 console.log("==================================================");
 
@@ -29,22 +32,41 @@ fs.readFile('./package-sample.json', 'utf8', (err, data) => {
   });
 });
 
-fs.readFile('./config/value.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  let cfg = JSON.parse(data);
-  cfg.type = packageType;
-  for (let e of cfg.engines) {
-    e.apiBaseUrl = apiBaseUrl;
-  }
-  let content = JSON.stringify(cfg, undefined, 2);
-  fs.writeFile('./config/value.json', content, writeErr => {
-    if (writeErr) {
-      console.error(writeErr);
-    } else {
-      // file written successfully
+if (packageValueFile) {
+  fs.readFile(packageValueFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
     }
+    fs.writeFile('./config/value.json', data, writeErr => {
+      if (writeErr) {
+        console.error(writeErr);
+      } else {
+        // file written successfully
+      }
+    });
   });
-});
+} else {
+  fs.readFile('./config/value.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    let cfg = JSON.parse(data);
+    cfg.type = packageType;
+    cfg.signup = `${webBaseUrl}/register`;
+    cfg.forgetPassword = `${webBaseUrl}/login?step=forgot-password`;
+    cfg.docs = `${webBaseUrl}/code/docs`;
+    for (let e of cfg.engines) {
+      e.apiBaseUrl = apiBaseUrl;
+    }
+    let content = JSON.stringify(cfg, undefined, 2);
+    fs.writeFile('./config/value.json', content, writeErr => {
+      if (writeErr) {
+        console.error(writeErr);
+      } else {
+        // file written successfully
+      }
+    });
+  });
+}
